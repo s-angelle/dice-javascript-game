3cc3be4ad6a895b6d4e29ea104d198580cbda086
Sabrina A <sabrinaangelle@gmail.com>
2024-03-13 14:43:51 +0900

    uploading initial project

---
+++ index.js
@@ -0,0 +1,3 @@
+const randomNumber = Math.floor(Math.random() * 7 );
+const playerOneImage = document.getElementById('playerOneImage');
+const playerTwoImage = document.getElementById('playerTwoImage');
